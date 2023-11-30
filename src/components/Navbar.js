df010d2c77e36ba93600c20438714495011682ca
A Omkar G Prabhu <prabhuomkar9@gmail.com>
2023-11-30 22:07:05 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 export default function Navbar(props) {
     return (
